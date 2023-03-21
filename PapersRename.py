bc8935cdc5eb1a94fcbec7671680c94c04adb69c
Berusska <karasek.s54@gmail.com>
2023-03-21 21:16:33 +0100

    Z úterý

--- PapersRename.py
+++ PapersRename.py
@@ -2,6 +2,9 @@ from bs4 import BeautifulSoup
 import pyperclip as pc
 import pyautogui
 import time
+import keyboard
+import PySimpleGUI as sg
+
 
 # https://www.sciencedirect.com/science/article/abs/pii/S1359511308001876
 url = "https://www.sciencedirect.com/science/article/abs/pii/S1359511308001876"
@@ -48,3 +51,68 @@ pq = PyQuery(html)
 tag = pq('title') # or     tag = pq('div.class')
 print(tag.text())
 
+
+
+from urllib.parse import urlparse
+web = urlparse("https://link.springer.com/article/10.1007/s00204-016-1827-3")
+web = urlparse("https://www.researchgate.net/publication/11211492_Influence_of_the_drying_technique_on_theophylline_pellets_prepared_by_extrusion-spheronization")
+web.scheme
+web.netloc #
+web.hostname
+
+
+
+
+def downloadPDF():
+    ...
+
+
+def userIO():
+    klavesa = keyboard.read_key()
+    
+    if klavesa == "esc":
+        print("Uživatelem vyžádaný konec programu")
+        sg.popup_auto_close("KONEC PROGRAMU.", background_color="darkgreen" ,auto_close_duration=1, keep_on_top=True, )
+        pyautogui.click(100, 200) #na zavření tabu
+        pyautogui.hotkey('ctrl', 'w')                 
+        break
+    elif klavesa == "f9": #PRO 
+        pyautogui.hotkey('f6')
+        pyautogui.hotkey('ctrl', 'c')    
+        schranka = pc.paste()
+        return 1
+    elif klavesa == "f8": 
+        pyautogui.hotkey('f6')
+        pyautogui.hotkey('ctrl', 'c')    
+        schranka = pc.paste()
+    
+        if predtim != schranka:
+            pyautogui.click(0, 200)
+            pyautogui.hotkey('ctrl', 's')#takže by mělo stačit jen zkopírovat F6; Ctrl + C 
+            pyautogui.click(100, 200) # stahovací okno musí mít určitou pozici
+            pyautogui.hotkey("enter") #na zavření dialogi stahování
+            predtim = schranka
+            indikace_schranky = 1 
+
+    
+def getTitle():
+    ...
+    
+def Rename():
+    ...
+
+
+
+while True:
+    if userID() == 0:
+        break
+    else:
+        nazev = getTitle()
+        web = ...
+        if web in ["link.springer.com", "www.researchgate.net", ...]:
+            downloadPDF()
+            
+        if novyFile:
+            Rename()
+        
+        
