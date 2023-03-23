f68ff1defe76b1eaf9c211031dccdc51bb15d894
Berusska <karasek.s54@gmail.com>
2023-03-23 13:07:51 +0100

    getTitle

--- PapersRename.py
+++ PapersRename.py
@@ -1,55 +1,6 @@
-from bs4 import BeautifulSoup
-import pyperclip as pc
-import pyautogui
-import time
-import keyboard
-import PySimpleGUI as sg
-
 
 # https://www.sciencedirect.com/science/article/abs/pii/S1359511308001876
 url = "https://www.sciencedirect.com/science/article/abs/pii/S1359511308001876"
-
-
-
-pyautogui.hotkey('ctrl', 'u') # TODO: pak url =  view-source:https://link.springer.com/article/10.1007/s00204-016-1827-3
-time.sleep(0.5) 
-pyautogui.hotkey('ctrl', 'a')  
-pyautogui.hotkey('ctrl', 'c')  
-pyautogui.hotkey('ctrl', 'w')  
-
-html = pc.paste()
-
-
-#buď
-start = html.find("""<meta name="citation_title" content=""") + 36
-konec = html[start:].find(" />")
-html[start:start+konec]
-
-
-#nebo
-start = html.find("<title>") + 7
-konec = html.find("</title>")
-html[start:konec]
-
-pyautogui.click(100, 200) #na zavření tabu
-pyautogui.hotkey('ctrl', 'w')  
-
-#anebo
-soup = BeautifulSoup(html, 'html.parser')
-
-print("Title of the website is : ")
-for title in soup.find_all('title'):
-    print(title)
-
-soup.find_all('title')[0].text
-
-
-
-#alebo
-from pyquery import PyQuery 
-pq = PyQuery(html)
-tag = pq('title') # or     tag = pq('div.class')
-print(tag.text())
 
 
 
@@ -64,6 +15,13 @@ web.hostname
 
 
 from pathlib import Path
+from pyquery import PyQuery 
+import pyperclip as pc
+import time
+import keyboard
+import PySimpleGUI as sg
+import pyautogui
+
 
 
 def downloadOpenPDF():
@@ -79,7 +37,17 @@ def downloadSciencePDF():
     ...
 
 def getTitle():
-    ...
+    pyautogui.hotkey('ctrl', 'u') # TODO: pak url =  view-source:https://link.springer.com/article/10.1007/s00204-016-1827-3
+    time.sleep(0.5) 
+    pyautogui.hotkey('ctrl', 'a')  
+    pyautogui.hotkey('ctrl', 'c')  
+    pyautogui.hotkey('ctrl', 'w') 
+    htmlWebu = pc.paste() 
+    
+    pq = PyQuery(htmlWebu)
+    tag = pq('title') # or     tag = pq('div.class')
+    Titul = tag.text()
+    return Titul
     
 def FileOccured():
     ...
