2bcc85d76ad1fa3490c67e386d00dbd425b07d7c
Berusska <karasek.s54@gmail.com>
2023-04-02 16:39:10 +0200

    Neděle

--- PapersRename.py
+++ PapersRename.py
@@ -15,9 +15,27 @@ import pyautogui
 import re
 import textwrap
 from urllib.parse import urlparse
+import fitz
+import pywhatkit
 
 
 import colorama
+
+colorama.init()
+theme = "Darkgreen"
+font_family = "Comic Sans MS"
+font_size = int(10)
+sg.theme(theme)
+sg.set_options(font=(font_family, font_size))
+
+primSlozka = Path.cwd()
+sekSlozka = Path("./downloaded")
+sekSlozka.mkdir(parents=True, exist_ok=True)
+
+veSlozce = set(sorted(primSlozka.glob("*.pdf"))) 
+
+with open("urls.txt", "r", encoding="utf-8") as f:
+    urls = f.read().splitlines()
 
 
 
@@ -33,7 +51,7 @@ def downloadOpenPDF():
 def downloadSciencePDF():
     ...  #TODO: zatím nechat
 
-def getTitle():
+def getTitle(science : bool):
     pyautogui.hotkey('ctrl', 'u') # pak lze url =  view-source:https://link.springer.com/article/10.1007/s00204-016-1827-3
     time.sleep(1) 
     pyautogui.click(100, 200)
@@ -49,6 +67,7 @@ def getTitle():
     print("\ttag:" + str(tag))
     Titul = tag.text()
     print("\ttitul: " + Titul)
+    
     return Titul
     
 def FileOccured(veSlozce: any):
@@ -79,32 +98,55 @@ def Rename(cestaFile: Path, titul: str):
         FileExistsError
 
 def hlaseniKonce():
-    layout = [[sg.Text(text='Byla stisknuta klávesa Esc.\n\nProgram byl ukončen uživatelem.', font=('Arial Bold', 20), background_color="indigo")]]
-    hlaseni = sg.Window("Konec programu.", layout,auto_close_duration=1, keep_on_top=True, size=(715,250), finalize=True, auto_close=True, background_color="indigo")
+    layout = [[sg.Text(text='Byla stisknuta klávesa Esc.\n\nProgram byl ukončen uživatelem.', font=('Arial Bold', 14), background_color="blue")]]
+    hlaseni = sg.Window("Konec programu.", layout,auto_close_duration=1, keep_on_top=True, size=(500,100), finalize=True, auto_close=True, background_color="blue")
 
     hlaseni.read()
     
+
 def hlaseniSouboru(newFile: Path, schranka: str, titul: str):
-    cestaFile = list(newFile)[0]
-    print(f"\t{cestaFile.name}")
-    schrana = textwrap.fill(schranka, 70).replace("\n", "\n\t")
+    print(f"\t{newFile.name}")
+    
+    # Load the first page of the PDF file
+    pdf_file = newFile
+    doc = fitz.open(pdf_file)
+    page = doc.load_page(0)
 
-    layout = [[sg.Text(f"Ve schránce je nová url adresa a zároveň se objevil nový PDF soubor v primární složce.\n\n\tNový soubor: {cestaFile.name}\n\n\tObsah schránka: {schrana}\n")],
-              [[sg.Button('V pořádku', size = (15, 2), pad=((15,20),20), button_color="green"), sg.Button('Špatně\nUprava uživatelem', size = (15, 2), pad=((15,20),20), button_color="red")]]]
+    # Convert the page to an image buffer
+    # pixmap = page.get_pixmap()
+    # img_bytes = pixmap.tobytes()
     
-    # schranka = pc.paste() #joko možnost opravy v průběhu
-    
-    schvaleni, other = sg.Window('Stahování zdrojů pro DP', layout, keep_on_top=True, size=(715,250), finalize=True, auto_close=True, background_color="indigo").read(close=True) 
-    
-    if schvaleni == 'Stahování zdrojů pro DP':
-        return True
-    else: 
-        return False
+    resolution = 60
 
-def UpravaUzivatelem():
-    print("Uprava uživatelem zatím není podporovaná.")
-    ... #TODO:
+    # Get the pixmap with the desired resolution
+    mat = fitz.Matrix(resolution / 72.0, resolution / 72.0)
+    pix = page.get_pixmap(matrix=mat)
+
+    # Convert the pixmap to bytes
+    img_bytes = pix.tobytes()
+
+
     
+    # ------ GUI Definition ------ #
+    pdfviewer = [[sg.Image(data=img_bytes)]]
+    formular = [[sg.T("Nový soubor:", s=11, justification="r"), sg.Multiline(size = (40, 5), key="-IN-", default_text=newFile.name), sg.FolderBrowse()], [sg.T("Obsah schranky:", s=11, justification="r"), sg.Multiline(size = (40, 5), key="-OUT-", default_text=schranka)], [sg.T("Titul:", s=11, justification="r"), sg.Multiline(size = (40, 5), key="-TIT-", default_text=titul)], [sg.Exit(s=16, button_color="tomato"), sg.B("V pořádku", size = (15, 2), pad=((15,20),20), button_color="green")]]
+
+    layout = [[sg.Text(f"Ve schránce je nová url adresa a zároveň se objevil nový PDF soubor v primární složce.\n")], [sg.Column(pdfviewer), sg.VSeperator(), sg.Column(formular)]]
+    window = sg.Window("GUI", layout, alpha_channel=0.99, grab_anywhere=True, location=(0,0))
+
+    while True:
+        event, values = window.read()
+        print(event)
+        print(values)
+        if event in (sg.WINDOW_CLOSED, "Exit"):
+            break
+        if event in ("Command 1", "Command 2", "Command 3", "Command 4"):
+            sg.popup_error("Not yet implemented")
+        if event == "V pořádku":
+            if is_valid_path(values["-IN-"]):
+                Rename(newFile, values["-TIT-"])
+
+    window.close()
     
 def SciencePortal(urlAdresa: str):
     web = urlparse(urlAdresa)
@@ -118,10 +160,12 @@ def SciencePortal(urlAdresa: str):
     else:
         return False
 
+def Zotero(pos):
+    pyautogui.click(pos)
+    time.sleep(1)
 
-def userIO():
+def userIO(pos):
     pc.copy("0"); predtim = "0"
-
     klavesa = keyboard.read_key()
     
     if klavesa == "esc":
@@ -129,18 +173,19 @@ def userIO():
         hlaseniKonce()
         pyautogui.click(100, 200) #na zavření tabu
         pyautogui.hotkey('ctrl', 'w')  
-        return (0, None, None)  #vede ke konci programu              
-    elif klavesa == "f9": #PRO ELSEVIER ETC. #TODO: místo uživatelsky volenou klávesou by se mohla název stránky: jestli bude náležet do množiny, pak to pojede podle následujícího kódu -> -> -> fce SciencePortal()
+        quit()
+
+    elif klavesa == "f9":
         print("\tStisknuta F9")
         pyautogui.click(100, 200)
         pyautogui.hotkey('f6')
         pyautogui.hotkey('ctrl', 'c')    
         schranka = pc.paste()
         
-        # downloadSciencePDF() 
-        #TODO: tady to stahování zatím nech být - bude to možná komplikovaný, zajisti jen to přejmenování
-        
-        name = getTitle()
+
+        name = getTitle(SciencePortal(schranka))
+            #TODO: tady to stahování zatím nech být - bude to možná komplikovaný, zajisti jen to přejmenování
+
         print("\tZiskan titul ")
         print(name)
         
@@ -150,16 +195,10 @@ def userIO():
             if newFile[0]:
                 # veSlozce = newFile[2]
                 print("\tNalezen nový soubor: " + str(newFile[1]))
-                schvaleni = hlaseniSouboru(newFile = newFile, schranka = schranka, titul=name)
-                
-                if schvaleni:
-                    Rename(newFile[1], name)
-                else:
-                    UpravaUzivatelem()
-                                
+                hlaseniSouboru(newFile = newFile[1], schranka = schranka, titul=name)
                 break
                     
-        return ("continue", newFile[2] )# vede k pokračování programu
+        return newFile[2]# vede k pokračování programu
         
     elif klavesa == "f8": #pro další bezejmenná otevřená pdf
         pyautogui.hotkey('f6')
@@ -170,54 +209,46 @@ def userIO():
             downloadOpenPDF()
             predtim = schranka
             
-            return ("continue", None) #TODO: nahradit None
+            while True:
+                newFile = FileOccured(veSlozce = veSlozce)
+                
+                if newFile[0]:
+                    # veSlozce = newFile[2]
+                    print("\tNalezen nový soubor: " + str(newFile[1]))
+                    hlaseniSouboru(newFile = newFile[1], schranka = schranka, titul="Soubor")
+                    break
+            
+            return newFile[2]
         
-    elif klavesa == "f6": #pro již stažená (nepohlídané vědecké portály)
-        pyautogui.hotkey('ctrl', 'c')    
-        schranka = pc.paste()
-    else: pass #není zmážknuta klávesa, nebo je jiná od definovaných
 
     
     #TODO: zajistit průběžný záznam práce do urls.txt
 
+def is_valid_path(filepath):
+    if filepath and Path(filepath).exists():
+        return True
+    
+    sg.popup_error("Filepath not correct")
+    return False
 
-def main():
-    print("\033[91m" + "Skript byl spuštěn." + "\033[0m")
+def NajdiZotero():
+    print("Najeďtě myší na ikonku Zotera ve svém prohlížeči. \nPonechte kurzor myši na ikoně a stiskněte klavesu CTRL, program bude pokračovat...")
+    pywhatkit.search("www.google.com")
     while True:
-        output = userIO()
-        if output[0] == 0:
+        pos = pyautogui.position()
+        klavesa = keyboard.read_key()
+        if klavesa == "ctrl":
+            print(f"Byla zjištěna poloha ikonky Zotera: {pos}.\n\nNyní můžete pracovat.")
             break
-        else:
-            veSlozce = output[1]
     
-    print("Vystoupení z while-loopu. Konec programu")
-    
-        
+    return pos
+
+
 if __name__ == "__main__":
-    colorama.init()
-
-    primSlozka = Path.cwd()
-    sekSlozka = Path("./downloaded")
-    sekSlozka.mkdir(parents=True, exist_ok=True)
-
-    veSlozce = set(sorted(primSlozka.glob("*.pdf"))) 
-    
-    with open("urls.txt", "r", encoding="utf-8") as f:
-        urls = f.read().splitlines()
-    
-    main()
-    
-    
-    
-#FIXME: https://pypi.org/project/PySimpleGUI/4.17.0/
-# TODO: sg.InputText() S DEFINOVANOU HODNOTOU
-# https://github.com/PySimpleGUI/PySimpleGUI/issues/966
-
-#https://www.activestate.com/resources/quick-reads/how-to-install-python-packages-using-a-script/# https://stackoverflow.com/questions/74154603/how-to-add-a-field-in-one-text-input-by-clicking-a-button-in-pysimplegui
-
-#https://www.reddit.com/r/learnpython/comments/mosgc5/updating_text_with_pysimplegui/
+    print("\033[91m" + "Skript byl spuštěn." + "\033[0m")
+    pos = NajdiZotero()
+    while True:
+        userIO(pos)
+        veSlozce = set(sorted(primSlozka.glob("*.pdf"))) 
 
 
-
-
-#TODO: na začátku nechat uživatel ekliknout na ikonku Zotera v pc; do RunProgram dát příkaz na spuštění Zotera
